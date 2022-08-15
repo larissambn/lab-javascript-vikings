555a9879b7c50623400924f44f2d53633f4cd599
larissambn <larissameo1@gmail.com>
2022-08-15 20:02:13 -0300

    almost solved

--- src/viking.js
+++ src/viking.js
@@ -1,11 +1,78 @@
 // Soldier
-class Soldier {}
+class Soldier {
+    constructor(health, strength) {
+    this.health = health;
+    this.strength = strength;
+    this.attack = function(){return this.strength}
 
-// Viking
-class Viking {}
+    this.receiveDamage =
+    function(damage){this.health-=damage}
+
+    }
+  }
+// Vikings
+
+class Viking extends Soldier  {
+    constructor (name,health,strength) {
+          super(health, strength);
+          this.name = name;
+
+          this.battleCry = function(){return Odin onws you all};
+
+        }
+    }
+
+if (health > damage);  {
+console.log(${this.name} has received ${this.receiveDamage} points of damage);
+
+else
+console.log(${this.name} has died in act of combat); 
+}
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier {
+    constructor (health, strength) {
+        super(health, strength);
+        
+    }
+    if (health > damage);  {
+        console log (A Saxon has received ${this.receiveDamage} points of damage); } {
+        
+        else
+        console.log(A Saxon has died in combat); 
+        }
+
+}
 
 // War
-class War {}
+class War {
+    constructor(vikingArmyproperty,saxonArmyproperty){
+    this.vikingArmyproperty = [""];
+    this.saxonArmyproperty = [""];
+
+ //   this.addViking = 
+ //   function(push[1]){}
+
+  //  for (
+  //  push.viking[1];
+  //  return 
+
+
+    }
+}
+
+class Armies Attack extends War 
+  constructor (vikingAttack,saxonAttack,showstatus) {
+
+  this.vikingAttack = function() { forEach (saxon.receiveDamage == Viking.strength && Viking.receiveDamage == saxon.strength);
+for(saxon.strength = 0) remove.(${saxon});
+
+  }
+
+  this.saxonAttack = function () { forEach (viking.receiveDamage == saxon.strength && saxon.receiveDamage == viking.strength);
+for(viking.strength = 0) remove.(${viking});
+
+}
+
+}
+
